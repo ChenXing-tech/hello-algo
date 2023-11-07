2104fd984303c361c535b085f4a55694b6383a52
chenxing <chenxing@bailongma-inc.com>
2023-11-07 09:59:52 +0800

    feature: build_tree方法

---
+++ src/main/java/codes/java/algo/recursion/divide/build_tree.java
@@ -0,0 +1,33 @@
+package codes.java.algo.recursion.divide;
+
+import codes.java.structure.tree.TreeNode;
+import java.util.HashMap;
+
+/**
+ * @author chenxing@bailongma-inc.com
+ * @date 2023/11/6
+ * @description
+ */
+public class build_tree {
+
+  public TreeNode dfs(int[] preOrder ,HashMap<Integer, Integer> map, int i, int l, int r){
+    if (r - l < 0){
+      return null;
+    }
+    TreeNode root = new TreeNode(preOrder[i]);
+    int m = map.get(preOrder[i]);
+    root.left = dfs(preOrder, map, i , l, m - 1);
+    root.right = dfs(preOrder, map, i + 1 + m - l , m + 1, r);
+    return root;
+  }
+
+  public TreeNode buildTree(int[] preOrder ,int[] inOrder){
+    HashMap<Integer, Integer> inOrderheadMap = new HashMap<>();
+    for (int i = 0; i < inOrder.length - 1; i++) {
+      inOrderheadMap.put(inOrder[i], i);
+    }
+    TreeNode dfs = dfs(preOrder, inOrderheadMap, 0, 0, preOrder.length - 1);
+    return dfs;
+  }
+
+}
--- src/main/java/codes/java/structure/tree/TreeNode.java
+++ src/main/java/codes/java/structure/tree/TreeNode.java
@@ -7,8 +7,8 @@ package codes.java.structure.tree;
  */
 public class TreeNode {
   int val; // 节点值
-  TreeNode left; //左节点
-  TreeNode right; // 右节点
+  public TreeNode left; //左节点
+  public TreeNode right; // 右节点
   public TreeNode(int val) {
     this.val = val;
   }
